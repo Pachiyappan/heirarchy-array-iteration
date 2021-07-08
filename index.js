8574fc637718c2e2617bb3dff24592ba4dd3bae9
Pachiyappan <pachid12@gmail.com>
2021-07-08 13:26:15 +0530

    Added an index with a function

---
+++ index.js
@@ -0,0 +1,18 @@
+import {isEmpty} from 'lodash';
+
+const flattenNestedArray = (arr, keyIdentifier) => {
+  let flatArray = [];
+  arr.forEach(element => {
+    if (!isEmpty(element[keyIdentifier])) {
+      flatArray = [
+        ...flatArray,
+        ...flattenNestedArray(element[keyIdentifier], keyIdentifier),
+      ];
+    } else {
+      flatArray.push(element);
+    }
+  });
+  return flatArray;
+};
+
+export default flattenNestedArray;
